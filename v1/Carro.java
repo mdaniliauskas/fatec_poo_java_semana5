55e658467e1e818395ac0f9447edf86a647a2dd9
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2021-09-15 13:15:51 -0300

    Subindo exercícios

---
+++ v1/Carro.java
@@ -0,0 +1,21 @@
+public class Carro {
+    private String modelo = "Agile";
+    private int velocidade = 0;    
+    
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getVelocidade() {
+        return this.velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+}
